5c01c7bbf96d7036ab40795cd92161d620870594
Muriel de Souza Godoi <murielgodoi@gmail.com>
2022-10-04 00:17:28 -0300

    Greedy Search and GRASP Implementation

--- tsp.c
+++ tsp.c
@@ -1,5 +1,13 @@
-#include <stdio.h>
+/**
+ * @file tsp.c
+ * @author Muriel Godoi (muriel@utfpr.edu.br)
+ * @brief
+ * @version 0.1
+ * @date 2022-10-01
+ *
+ */
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <math.h>
 #include <time.h>
@@ -10,16 +18,16 @@ typedef struct
   float x;
   float y;
   float z;
-} Coordinate;
+} Coordenada;
 
 typedef struct
 {
-  char name[100];
-  char type[100];
+  char nome[100];
+  char tipo[100];
   char comment[100];
   int dimension;
   char edgeWeightType[20];
-  Coordinate *nodes;
+  Coordenada *nodes;
   float **distances;
 } Instance;
 
@@ -29,7 +37,7 @@ Instance readTspFile(char *fileName)
   char field[100];
   Instance instance;
 
-  printf("Abrindo arquivo %s\n",fileName);
+  printf("Abrindo arquivo %s\n", fileName);
   // Abre o arquivo .tsp
   FILE *file = fopen(fileName, "r");
   if (file == NULL)
@@ -39,33 +47,36 @@ Instance readTspFile(char *fileName)
   } // if
 
   // Lê os atributos da instancia
-  fscanf(file, "%*[^:]:%[^\n]\n", instance.name);
-  fscanf(file, "%*[^:]:%[^\n]\n", instance.type);
+  fscanf(file, "%*[^:]:%[^\n]\n", instance.nome);
+  fscanf(file, "%*[^:]:%[^\n]\n", instance.tipo);
   fscanf(file, "%*[^:]:%[^\n]\n", instance.comment);
-  do{
-    fscanf(file, "%[^:]:%[^\n]\n", field,buffer);
-  }while(strcmp(field,"COMMENT ")==0);
+  do
+  {
+    fscanf(file, "%[^:]:%[^\n]\n", field, buffer);
+  } while (strcmp(field, "COMMENT ") == 0);
   instance.dimension = strtol(buffer, NULL, 10);
   fscanf(file, "%*[^:]:%[^\n]\n", instance.edgeWeightType);
   fscanf(file, "%*[^\n]\n");
 
   // Aloca dinamicamente o vetor para a dimensão da instância
-  instance.nodes = (Coordinate *)malloc(sizeof(Coordinate) * instance.dimension);
+  instance.nodes = (Coordenada *)malloc(sizeof(Coordenada) * instance.dimension);
 
   // Lê os vértices das instancias e salva no vetor de nodos
-  printf("Lendo %d vertices...\n",instance.dimension);
+  printf("Lendo %d vertices...", instance.dimension);
   for (size_t i = 0; i < instance.dimension; i++)
   {
     fscanf(file, "%*d %f %f %f\n", &instance.nodes[i].x, &instance.nodes[i].y, &instance.nodes[i].z);
   } // for
+  printf("OK\n");
 
   // Aloca a metriz de distancias dinamicamente
-  printf("Alocando matriz de distancias...\n");
+  printf("Alocando matriz de distancias...");
   instance.distances = (float **)malloc(instance.dimension * sizeof(float *));
   for (int i = 0; i < instance.dimension; i++)
   {
     instance.distances[i] = (float *)malloc(instance.dimension * sizeof(float));
   } // for
+  printf("OK\n");
 
   printf("Calculando matriz de distancias...");
   // Calcula a matriz de distancias entre as estrelas
@@ -78,56 +89,20 @@ Instance readTspFile(char *fileName)
           pow(instance.nodes[i].y - instance.nodes[j].y, 2) +
           pow(instance.nodes[i].z - instance.nodes[j].z, 2));
     } // for
-    printf("\rCalculando matriz de distancias... %.2f%%",100*(float)i/instance.dimension);
+    printf("\rCalculando matriz de distancias... %.2f%%", 100 * (float)i / instance.dimension);
     fflush(stdout);
-  }   // for
-  printf("\rCalculando matriz de distancias... 100%% completo\n");
+  } // for
+  printf("\rCalculando matriz de distancias... 100%% OK\n");
 
   return instance;
 
 } // readTspFile
 
-int *geraRotaInicial(int tamanho)
-{
-  int trocaPos;
-  int troca;
-
-  int *rota = (int *)malloc(tamanho * sizeof(int));
-
-  // Inicia a rota em ordem
-  for (int i = 0; i < tamanho; i++)
-  {
-    rota[i] = i;
-  } // for
-
-  // Embaralha a rota aleatóriamente
-  for (int i = 1; i < tamanho; i++)
-  {
-    trocaPos = (rand() % 98)+1;
-    troca = rota[i];
-    rota[i] = rota[trocaPos];
-    rota[trocaPos] = troca;
-  } // for
-  return rota;
-} // geraRotaInicial método
-
-float fitness(Instance instance, int *rota)
-{
-  int i;
-  float soma = 0;
-  for (i = 0; i < instance.dimension - 1; i++)
-  {
-    soma += instance.distances[rota[i]][rota[i + 1]];
-  }
-  soma += instance.distances[rota[i]][rota[0]];
-  return soma;
-}
-
 Instance displayInstance(Instance instance)
 {
 
-  printf("Nome: %s\n", instance.name);
-  printf("Tipo: %s\n", instance.type);
+  printf("Nome: %s\n", instance.nome);
+  printf("Tipo: %s\n", instance.tipo);
   printf("Comentário: %s\n", instance.comment);
   printf("Dimensão: %d\n", instance.dimension);
   printf("Tipo de peso da aresta: %s\n", instance.edgeWeightType);
@@ -148,50 +123,202 @@ Instance displayInstance(Instance instance)
 
   return instance;
 
-} // readTspFile
+} // displayInstance method
 
-void opt2(Instance instance, int *rota, int interacoes)
+float fitness(Instance instance, int *rota)
 {
-  int node1;
-  int node2;
-  int troca;
-  float melhorDistancia = fitness(instance, rota);
-  float distancia;
-
-  for (int i = 0; i < interacoes; i++)
+  int i;
+  float soma = 0;
+  for (i = 0; i < instance.dimension - 1; i++)
   {
-    node1 = rand() % (instance.dimension - 1);
-    node2 = rand() % (instance.dimension - 1);
-
-    troca = rota[node1];
-    rota[node1] = rota[node2];
-    rota[node2] = troca;
-
-    distancia = fitness(instance, rota);
-    if (distancia < melhorDistancia)
-    {
-      melhorDistancia = distancia;
-      printf("Iteração %i - Distancia %10.2f\n", i, melhorDistancia);
-    }
-    else
-    {
-      troca = rota[node1];
-      rota[node1] = rota[node2];
-      rota[node2] = troca;
-    }
+    soma += instance.distances[rota[i]][rota[i + 1]];
   }
-}
+  soma += instance.distances[rota[i]][rota[0]];
+  return soma;
+} // fitness method
 
-void run2optReverse(int* rota, int inicio, int fim){
+int *geraRotaAleatoria(int tamanho)
+{
+  int trocaPos;
   int troca;
-  for (int i = inicio+1, j = fim; i < j; i++,j--)
+
+  int *rota = (int *)malloc(tamanho * sizeof(int));
+
+  // Inicia a rota em ordem
+  for (int i = 0; i < tamanho; i++)
+  {
+    rota[i] = i;
+  } // for
+
+  // Embaralha a rota aleatóriamente
+  for (int i = 1; i < tamanho; i++)
+  {
+    trocaPos = (rand() % 98) + 1;
+    troca = rota[i];
+    rota[i] = rota[trocaPos];
+    rota[trocaPos] = troca;
+  } // for
+  return rota;
+} // geraRotaAleatoria método
+
+int *geraRotaGulosa(Instance instance)
+{
+  printf("Gerando rota inicial gulosa....");
+  int *rota = (int *)malloc(instance.dimension * sizeof(int));
+  bool *visitados = (bool *)malloc(instance.dimension * sizeof(bool));
+  float distancia = 0;
+  float distProx;
+  int proximo = 0;
+  int i, j;
+
+  // Marca vertices como não visitados
+  visitados[0] = true;
+  for (i = 1; i < instance.dimension; i++)
+  {
+    visitados[i] = false;
+  } // for
+
+  // Para cada vertice
+  for (i = 0; i < instance.dimension; i++)
+  {
+    // Localiza o vértice mais próximo
+    distProx = INFINITY;
+    for (j = 1; j < instance.dimension; j++)
+    {
+      if (instance.distances[proximo][j] < distProx && !visitados[j])
+      {
+        distProx = instance.distances[proximo][j];
+        proximo = j;
+      } // if
+    }   // for
+
+    // Adiciona na rota
+    rota[i] = proximo;
+    visitados[proximo] = true;
+
+  } // for
+  printf("OK\n");
+
+  return rota;
+} // geraRotaGulosa
+
+int *geraRotaGrasp(Instance instance, float alpha)
+{
+  int *rota = (int *)malloc(instance.dimension * sizeof(int));
+  bool *visitados = (bool *)malloc(instance.dimension * sizeof(bool));
+
+  int i, j;
+  float min, max, corte;
+  int cardinalidade, sorteio, contaCandidatos;
+  int proximo = 0;
+
+  // Marca vertices como não visitados
+  visitados[0] = true;
+  for (i = 1; i < instance.dimension; i++)
+  {
+    visitados[i] = false;
+  } // for
+
+  // Para cada vertice
+  for (i = 0; i < instance.dimension - 1; i++)
+  {
+    max = -INFINITY;
+    min = INFINITY;
+
+    // Encontra distancia min e max
+    for (j = 0; j < instance.dimension; j++)
+    {
+      if (instance.distances[proximo][j] > max && !visitados[j])
+      {
+        max = instance.distances[proximo][j];
+      }
+      if (instance.distances[proximo][j] < min && !visitados[j])
+      {
+        min = instance.distances[proximo][j];
+      } // if
+    }   // for
+
+    // Calcula cardinalidade
+    corte = min + (alpha * (max - min));
+
+    cardinalidade = 0;
+    for (j = 0; j < instance.dimension; j++)
+    {
+      if (instance.distances[proximo][j] <= corte && !visitados[j])
+      {
+        cardinalidade++;
+      } // if
+    }   // for
+
+    sorteio = rand() % cardinalidade;
+
+    // Procura o valor sorteado
+    contaCandidatos = 0;
+    for (j = 0; j < instance.dimension; j++)
+    {
+      if (instance.distances[proximo][j] <= corte && !visitados[j])
+      {
+        if (contaCandidatos == sorteio)
+        {
+          proximo = j;
+          break;
+        }
+        contaCandidatos++;
+
+      } // if
+    }   // for
+    // Adiciona na rota
+    rota[i] = proximo;
+    visitados[proximo] = true;
+
+  } // for
+
+  return rota;
+} // geraRotaGrasp
+
+void run2optReverse(int *rota, int inicio, int fim)
+{
+  int troca;
+  for (int i = inicio + 1, j = fim; i < j; i++, j--)
   {
     troca = rota[i];
     rota[i] = rota[j];
     rota[j] = troca;
   }
-  
 }
+
+int runSa2opt(Instance instance, int *rota)
+{
+  int node1;
+  int node2;
+  int troca;
+  float distancia = fitness(instance, rota);
+  float delta;
+  bool melhorou = true;
+  int temperatura = 1000;
+
+  while (melhorou)
+  {
+    melhorou = false;
+    temperatura *= 0.95;
+    for (node1 = 0; node1 < instance.dimension; node1++)
+    {
+      for (node2 = node1 + 1; node2 < instance.dimension; node2++)
+      {
+        delta = -instance.distances[rota[node1]][rota[(node1 + 1) % instance.dimension]] - instance.distances[rota[node2]][rota[(node2 + 1) % instance.dimension]] + instance.distances[rota[node1]][rota[(node2) % instance.dimension]] + instance.distances[rota[(node1 + 1) % instance.dimension]][rota[(node2 + 1) % instance.dimension]];
+
+        if (delta < -0.00001 || ((float)(rand() % 1000) < temperatura))
+        {
+          run2optReverse(rota, node1, node2);
+          distancia += delta;
+          melhorou = true;
+        } // if
+      }   // for
+    }     // for
+    // printf("Distancia atual: %f Temperatura: %i\n", distancia, temperatura);
+  } // while
+  return distancia;
+} // run2opt
 
 int run2opt(Instance instance, int *rota)
 {
@@ -207,51 +334,61 @@ int run2opt(Instance instance, int *rota)
     melhorou = false;
     for (node1 = 0; node1 < instance.dimension; node1++)
     {
-      for (node2 = node1+1; node2 < instance.dimension; node2++)
+      for (node2 = node1 + 1; node2 < instance.dimension; node2++)
       {
-        delta = - instance.distances[rota[node1]][rota[(node1+1)%instance.dimension]]
-                - instance.distances[rota[node2]][rota[(node2+1)%instance.dimension]]
-                + instance.distances[rota[node1]][rota[(node2)%instance.dimension]]
-                + instance.distances[rota[(node1+1)%instance.dimension]][rota[(node2+1)%instance.dimension]];
+        delta = -instance.distances[rota[node1]][rota[(node1 + 1) % instance.dimension]] - instance.distances[rota[node2]][rota[(node2 + 1) % instance.dimension]] + instance.distances[rota[node1]][rota[(node2) % instance.dimension]] + instance.distances[rota[(node1 + 1) % instance.dimension]][rota[(node2 + 1) % instance.dimension]];
 
         if (delta < -0.00001)
         {
-          run2optReverse(rota,node1, node2);
+          run2optReverse(rota, node1, node2);
           distancia += delta;
           melhorou = true;
-         }//if
-      }// for
-    }// for
-  } // while
+        } // if
+      }   // for
+    }     // for
+  }       // while
   return distancia;
-}//run2opt
+} // run2opt
 
-//void saveTour(Instance instance, int* rota)
+// void saveTour(Instance instance, int* rota)
 
 int main(int argc, char **argv)
 {
-
-
-  Instance instance = readTspFile("data/star250k.tsp");
-  // displayInstance(instance);
   srand(time(NULL));
+  // srand(1);
+
   int *rota;
-  float menorTour = INFINITY;
   float distancia;
-  for (int i = 0; i < 1000000; i++)
+  float minDistancia = INFINITY;
+  Instance instance = readTspFile("data/star1k.tsp");
+  // displayInstance(instance);
+
+  for (size_t i = 0; i < 1000; i++)
   {
-    rota = geraRotaInicial(instance.dimension);
+    /* code */
+
+    // rota = geraRotaAleatoria(instance.dimension);
+    // rota = geraRotaGulosa(instance);
+    rota = geraRotaGrasp(instance, (rand()%10)/10.0);
+    distancia = fitness(instance, rota);
+    //printf("Distancia GRASP= %f\n", distancia);
+
     distancia = run2opt(instance, rota);
-    free(rota);
-    if(distancia < menorTour){
-      menorTour = distancia;
-      printf("\nTour = %f\n",menorTour);
+    distancia = fitness(instance, rota);
+    printf("Distancia 2-OPT= %f\n", distancia);
+
+    if (distancia < minDistancia)
+    {
+      minDistancia = distancia;
     }
-      printf("\rIteração = %i",i);
-      fflush(stdout);
-  }
 
+    // distancia = runSa2opt(instance, rota);
+    // printf("Distancia 2opt + SA = %f\n", distancia);
 
+    free(rota);
+  } // for
 
+  printf("Distancia Minima: %f\n", minDistancia);
 
+  return 0;
 } // main method
